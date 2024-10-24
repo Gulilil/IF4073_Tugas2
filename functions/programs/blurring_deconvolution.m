034f6487faa50e7047c63313f8f06eebe0b1d1f3
Gulilil <juan.csantoso@gmail.com>
2024-10-24 18:08:47 +0700

    commenting unnecessary lines

--- functions/programs/blurring_deconvolution.m
+++ functions/programs/blurring_deconvolution.m
@@ -2,6 +2,6 @@ function image_restored = blurring_deconvolution(img)
     % Menjalankan fungsi untuk Motion Blurring
     image_restored = motion_blur(img);
 
-    % Menampilkan gambar sebelum dan sesudah Motion Bluring
-    subplot(1, 2, 1); imshow(img); title("Original Image");
-    subplot(1, 2, 2); imshow(image_restored); title("Motion Blur Image");
+    % % Menampilkan gambar sebelum dan sesudah Motion Bluring
+    % subplot(1, 2, 1); imshow(img); title("Original Image");
+    % subplot(1, 2, 2); imshow(image_restored); title("Motion Blur Image");
--- functions/programs/image_smoothing_spatial.m
+++ functions/programs/image_smoothing_spatial.m
@@ -1,37 +1,37 @@
-function imgMeanFiltered = image_smoothing_spatial(img, spatialType)
+function imgMeanFiltered = image_smoothing_spatial(img, spatialType, nMask)
+    % spatialType = ["Mean", "Gaussian"]
     % Function to apply mean filter smoothing to an image with added noise
 
     % Masukkan jumlah nilai untuk pembentukan ukuran matriks mean
-    nMask = input("Masukkan ukuran matriks masking (n): ");
-    while mod(nMask, 2) == 0
-        fprintf("Masukkan bilangan ganjil\n");
-        nMask = input("Masukkan ukuran matriks masking (n): ");
-    end
+    if mod(nMask, 2) == 0
+        fprintf("[FAILED] Cannot process even number of n\n");
+    else 
 
-    % Proses untuk Image Smoothing dengan Spatial Type yang dipilih
-    fprintf("Image Smoothing dengan memanfaatkan " + spatialType + " Filter\n");
-    
-    % Add Gaussian noise to the image
-    imgWithNoise = imnoise(img, 'gaussian', 0.1);
-    
-    % Apply Mean filter using convolution
-    if spatialType == "Mean"
-        imgMeanFiltered = convolution(double(imgWithNoise), false, "mean", nMask);
-    elseif spatialType == "Gaussian"
-        imgMeanFiltered = convolution(double(imgWithNoise), false, "gaussian", nMask);
-    end
-    
-    % Display both images (noisy and filtered) side by side using subplot
-    figure;
-    subplot(1, 2, 1);
-    imshow(imgWithNoise);
-    title('Image with Noise');
-    
-    subplot(1, 2, 2);
-    imshow(imgMeanFiltered);
-    if spatialType == "Mean"
-        title('Image after Mean Filter Smoothing');
-    elseif spatialType == "Gaussian"
-        title('Image after Gaussian Filter Smoothing');
+        % Proses untuk Image Smoothing dengan Spatial Type yang dipilih
+        fprintf("[PROCESS] Image Smoothing using " + spatialType + " Filter\n");
+        
+        % Add Gaussian noise to the image
+        imgWithNoise = imnoise(img, 'gaussian', 0.1);
+        
+        % Apply Mean filter using convolution
+        if spatialType == "Mean"
+            imgMeanFiltered = convolution(double(imgWithNoise), false, "mean", nMask);
+        elseif spatialType == "Gaussian"
+            imgMeanFiltered = convolution(double(imgWithNoise), false, "gaussian", nMask);
+        end
+        
+        % % Display both images (noisy and filtered) side by side using subplot
+        % figure;
+        % subplot(1, 2, 1);
+        % imshow(imgWithNoise);
+        % title('Image with Noise');
+        % 
+        % subplot(1, 2, 2);
+        % imshow(imgMeanFiltered);
+        % if spatialType == "Mean"
+        %     title('Image after Mean Filter Smoothing');
+        % elseif spatialType == "Gaussian"
+        %     title('Image after Gaussian Filter Smoothing');
+        % end
     end
 end
