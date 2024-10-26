868acfc5f63f4109d3e20701d223e9bb7ac9be11
Gulilil <juan.csantoso@gmail.com>
2024-10-26 17:10:39 +0700

    integrate no 7

--- functions/programs/blurring_deconvolution.m
+++ functions/programs/blurring_deconvolution.m
@@ -1,21 +1,12 @@
-function image_blurred = blurring_deconvolution(img, len, theta)
+function [image_restored, image_blurred, original_vs_restored] = blurring_deconvolution(img, len, theta)
     % Menjalankan fungsi Motion Blurring
     [blur_processing, image_blurred] = motion_blur(img, len, theta);
-
-    % Menampilkan gambar original and motion-blurred
-    subplot(2, 3, 1); imshow(img); title("Original Image");
-    subplot(2, 3, 2); imshow(image_blurred); title("Motion Blur Image");
     
     % Menjalankan fungsi untuk Deblurring dengan penapis Weiner
     noise_var = 0.0001;
-    wiener_result = wiener_deconvolution_scratch(img, noise_var, blur_processing, image_blurred);
+    image_restored = wiener_deconvolution_scratch(img, noise_var, blur_processing, image_blurred);
     
-    % Menampilkan gambar yang sudah diperbaiki secara Scratch
-    subplot(2, 3, 3); imshow(wiener_result); title("Restored Image (Scratch)");
-
     % Konversi image ke dalam bentuk double
     img_converted = im2double(img);
-    
-    % Menghitung perbedaan absolut antara gambar original dengan gambar yang diperbaiki
-    subplot(2, 3, 6); imshow(imabsdiff(img_converted, wiener_result)); title('Original VS Restored Image (Scratch)');
+    original_vs_restored = imabsdiff(img_converted, image_restored);
 end
--- functions/wiener/wiener_deconvolution_scratch.m
+++ functions/wiener/wiener_deconvolution_scratch.m
@@ -16,14 +16,6 @@ function wiener_result = wiener_deconvolution_scratch(img, noise_var, blur_proce
         % Jika gambar grayscale, langsung proses
         wiener_result = wiener_calculation(img, noise_var, blur_processing, image_blurred);
     end
-
-    % Menjalankan Dekonvolusi dengan library Wiener Filter
-    % estimated_nsr = noise_var / var(double(img(:))); % Equivalent to Sη(u,v) / Sf(u,v)
-    wiener_result_lib = deconvwnr(image_blurred, blur_processing, noise_var);
-    subplot(2, 3, 4); imshow(wiener_result_lib); title("Restored Image (Library)");
-
-    % Menghitung perbedaan absolut antara gambar original dengan gambar yang diperbaiki (Library)
-    subplot(2, 3, 5); imshow(imabsdiff(img, wiener_result_lib)); title('Original VS Restoring Image (Library)')
     
     % Scale gambar ke dalam bentuk gray 0-1
     wiener_result = mat2gray(wiener_result);
