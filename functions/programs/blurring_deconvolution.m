d97ef20fd1d8fe798e627d5f6d691c55622ecca8
Gulilil <juan.csantoso@gmail.com>
2024-10-26 18:01:08 +0700

    add noise var param no 7:

--- functions/programs/blurring_deconvolution.m
+++ functions/programs/blurring_deconvolution.m
@@ -1,9 +1,8 @@
-function [image_restored, image_blurred, original_vs_restored] = blurring_deconvolution(img, len, theta)
+function [image_restored, image_blurred, original_vs_restored] = blurring_deconvolution(img, len, theta, noise_var)
     % Menjalankan fungsi Motion Blurring
     [blur_processing, image_blurred] = motion_blur(img, len, theta);
     
     % Menjalankan fungsi untuk Deblurring dengan penapis Weiner
-    noise_var = 0.0001;
     image_restored = wiener_deconvolution_scratch(img, noise_var, blur_processing, image_blurred);
     
     % Konversi image ke dalam bentuk double
--- functions/programs/periodic_noise_restoration.m
+++ functions/programs/periodic_noise_restoration.m
@@ -1,15 +1,12 @@
-function result_img = periodic_noise_restoration(img, filter_type)
+function [result_img, fourierDisplay, meshArrays] = periodic_noise_restoration(img, filter_type)
 
-imshow(img);
 [M, N, n_channel] = size(img);
-imshow(img);
 title('original image');
 
 % Lakukan transformasi Fourier pada f(x, y)
 % Display the Fourier Spectrum
 [fourierSpectrum, fourierDisplay] = fourier_transform(M, N, n_channel, img);
-figure, imshow(fourierDisplay,[]); title('Fourier spectrum');
 
-meshArrays = filter_processing(frequencyType, M, N, 0, 0, 0, 0); % Ignore cutoff Frequency, n, gammaL, and gammaH
-
+meshArrays = filter_processing(filter_type, M, N, 0, 0, 0, 0); % Ignore cutoff Frequency, n, gammaL, and gammaH
+result_img = img;
 end
